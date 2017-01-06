a08226acb5715907b752ac703cdab7dc712a8703
Maksym Tymtsiv <mtymtsiv@gmail.com>
2017-01-06 10:57:52 -0500

    Initial commit

--- final/settings.py
+++ final/settings.py
@@ -84,13 +84,11 @@ USE_L10N = False
 
 USE_TZ = True
 
+DATABASES['default'].update(db_from_env)
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 
 # DATABASES['default'] =  dj_database_url.config()
-
-
-DATABASES['default'].update(db_from_env)
 
 LOGIN_REDIRECT_URL = '/home/'
 
