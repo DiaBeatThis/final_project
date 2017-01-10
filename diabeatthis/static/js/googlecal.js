7351cd0d856feecfd444f81c87897fb8bbfef654
Maksym Tymtsiv <mtymtsiv@gmail.com>
2017-01-10 15:15:49 -0500

    pull

--- diabeatthis/static/js/googlecal.js
+++ diabeatthis/static/js/googlecal.js
@@ -27,7 +27,7 @@ function handleAuthResult(authResult) {
   if (authResult && !authResult.error) {
 	// Hide auth UI, then load client library.
 	authorizeDiv.style.display = 'none';
-	loadCalendarApi();
+	add_event();
   } else {
 	// Show auth UI, allowing the user to initiate authorization by
 	// clicking authorize button.
@@ -54,7 +54,7 @@ function handleAuthClick(event) {
  */
 
 function loadCalendarApi() {
-  gapi.client.load('calendar', 'v3', listUpcomingEvents);
+  gapi.client.load('calendar', 'v3', add_event);
 }
 
 /**
@@ -102,3 +102,52 @@ function appendPre(message) {
   var textContent = document.createTextNode(message + '\n');
   pre.appendChild(textContent);
 }
+
+
+var new_event = {
+  'summary': 'Google I/O 2015',
+  'location': '800 Howard St., San Francisco, CA 94103',
+  'description': 'A chance to hear more about Google\'s developer products.',
+  'start': {
+    'dateTime': '2017-01-12T09:00:00-07:00',
+    'timeZone': 'America/Los_Angeles'
+  },
+  'end': {
+    'dateTime': '2017-01-13T17:00:00-07:00',
+    'timeZone': 'America/Los_Angeles'
+  },
+  'recurrence': [
+    'RRULE:FREQ=DAILY;COUNT=2'
+  ],
+  'attendees': [
+    {'email': 'lpage@example.com'},
+    {'email': 'sbrin@example.com'}
+  ],
+  'reminders': {
+    'useDefault': false,
+    'overrides': [
+      {'method': 'email', 'minutes': 24 * 60},
+      {'method': 'popup', 'minutes': 10}
+    ]
+  }
+};
+function add_event(){
+var request = gapi.client.calendar.events.insert({
+  'calendarId': 'primary',
+  'resource': new_event
+});
+
+request.execute(function(new_event) {
+  appendPre('Event created: ' + new_event.htmlLink);
+});
+}
+
+// var crequest = gapi.client.calendar.events.insert({
+//  'calendarId': 'primary',
+//  'resource': event
+// });
+//
+// crequest.execute(function(event) {
+//  appendPre('Event created: ' + event.htmlLink);
+// });
+// }
--- diabeatthis/static/js/script.js
+++ diabeatthis/static/js/script.js
@@ -51,13 +51,40 @@ getWater()
 getSteps()
 getLastWeekInsulin()
 lastXDays()
-
+getDateWeekStarts()
+console.log($('#insulinWeek').val())
 // <!-- updating charts for date -->
 function chartsForDate (){
     insulinByDate()
     getGlucose()
     getWater()
     getSteps()
+}
+
+function getDateWeekStarts(){
+    year = ($('#insulinWeek').val()).slice(0, 4)
+    week = parseInt(($('#insulinWeek').val()).slice(6, 8))
+    // Jan 1 of 'year'
+    var d = new Date(year, 0, 1),
+        offset = d.getTimezoneOffset();
+
+    // ISO: week 1 is the one with the year's first Thursday
+    // so nearest Thursday: current date + 4 - current day number
+    // Sunday is converted from 0 to 7
+    d.setDate(d.getDate() + 4 - (d.getDay() || 7));
+
+    // 7 days * (week - overlapping first week)
+    d.setTime(d.getTime() + 7 * 24 * 60 * 60 * 1000
+        * (week + (year == d.getFullYear() ? -1 : 0 )));
+
+    // daylight savings fix
+    d.setTime(d.getTime()
+        + (d.getTimezoneOffset() - offset) * 60 * 1000);
+
+    // back to Monday (from Thursday)
+    d.setDate(d.getDate() - 3);
+    console.log(d)
+    return d;
 }
 
 // <!-- getting results for past 7 days -->
@@ -254,6 +281,7 @@ function waterCharts(){
         yAxis: {
             allowDecimals: false,
             min: 0,
+            max: 15,
             title: {
                 text: 'Ounces'
             },
