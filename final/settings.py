7a4359ce7b80802f7858ce7ccdfb87ab01cc35d0
Maksym Tymtsiv <mtymtsiv@gmail.com>
2017-01-10 10:24:32 -0500

    Initial commit

--- final/settings.py
+++ final/settings.py
@@ -2,7 +2,7 @@ import os
 # from .secrets import SECRET_KEY, DATABASES
 import dj_database_url
 
-# SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ['SECRET_KEY']
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
