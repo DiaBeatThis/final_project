0cef8fb373f9e85cf41d6821c2c70eefa4137ad6
Z-Ferguson <zfergusonn@gmail.com>
2017-01-11 11:00:23 -0500

    merging

--- final/settings.py
+++ final/settings.py
@@ -9,11 +9,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-<<<<<<< HEAD
-ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'mighty-wave-77740.herokuapp.com', 'diabeatthis.herokuapp.com']
-=======
+
 ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'diabeatthis.herokuapp.com']
->>>>>>> 5220d187be2b809a0dc39a0333250c7a00c03283
 
 # Application definition
 
